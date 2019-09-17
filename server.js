dc26b6ad9222e67c85077e8713dc00441c6be6e8
pavanesh2019 <49507640+pavanesh2019@users.noreply.github.com>
2019-09-17 20:25:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ const path = require('path');
 
 const port = 3000;
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angualr8-sept-2019'));
-
-app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/angualr8-sept-2019/index.html'));
+app.get('*', (req, res) => {
+ 
+    // angular routing
+    console.log(`Angular serving ${req.url}`);
+    res.sendFile(path.resolve('angualr8-sept-2019/index.html'));
+  
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => console.log(`Listening on port ${port}!`));
