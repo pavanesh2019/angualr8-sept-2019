fbc4a1c147b69deec92d11d5bd6ac2a2c2076db2
pavanesh2019 <49507640+pavanesh2019@users.noreply.github.com>
2019-09-17 13:06:09 +0530

    CORS module added

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angualr8-sept-2019'));
