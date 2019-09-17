70ddbbe550a18cebdcfef31b3bf7987560d18106
pavanesh2019 <49507640+pavanesh2019@users.noreply.github.com>
2019-09-17 20:15:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-//Install express server
-/*const express = require('express');
-const path = require('path');
-const cors = require('cors');
-
-var corsOptions = {
-  origin: 'https://angular8-sept-2019.herokuapp.com/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
+const express = require('express');
 const app = express();
 
-app.use(cors(corsOptions));
+
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
+
+const path = require('path');
+
+const port = 3000;
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/public/angualr8-sept-2019'));
 
-app.get('/*', function(req,res) {
-
+app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/public/angualr8-sept-2019/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);*/
-const express = require('express');  
-const app = express();  
-app.use(express.static(__dirname + '/dist'));  
-app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
-});  
-app.listen(process.env.PORT || 8080);  
+app.listen(process.env.PORT || 8080);
